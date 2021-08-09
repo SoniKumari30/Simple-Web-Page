3f00a1b31e8ef9e8d7c86de23d77a6de728f091c
soni kumari <67049547+SoniKumari30@users.noreply.github.com>
2021-08-09 20:44:39 +0530

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const menuLinks = document.querySelector('.navbar__menu')
 const navLogo = document.querySelector('#navbar__logo')
-
+//
 // Show active menu when scrolling
 
 const highlightMenu = () => {
@@ -9,7 +9,7 @@ const highlightMenu = () => {
   const aboutMenu = document.querySelector('#about-page')
   const servicesMenu = document.querySelector('#services-page')
   let scrollPos = window.scrollY
-
+//
   // adds 'highlight' class to my menu items
   
   if (window.innerWidth > 960 && scrollPos < 600) {
