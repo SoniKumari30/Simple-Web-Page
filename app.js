ab37bb84f8037265c12d64ca59ddfdec1612a93a
soni kumari <67049547+SoniKumari30@users.noreply.github.com>
2021-08-09 20:56:17 +0530

    first commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const highlightMenu = () => {
   if (window.innerWidth > 960 && scrollPos < 600) {
     homeMenu.classList.add('highlight');
     aboutMenu.classList.remove('highlight');
-    return;//
+    return;
   } else if (window.innerWidth > 960 && scrollPos < 1400) {
     aboutMenu.classList.add('highlight');
     homeMenu.classList.remove('highlight');
